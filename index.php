32551e257dcc45784b4a3352c5b38b16a069f628
iSajmon <131696692+iSajmon@users.noreply.github.com>
2024-12-02 15:30:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>soneque</title>
+</head>
+<body>
+    <header>
+        <h1>soneque</h1>
+        <div class="links">
+            <a href="">o marce</a>
+            <a href="">kontakt</a>
+            <a href="">opinie klientów</a>
+        </div>
+    </header>
+    <main>
+        <div class="products">
+            <h2>PRODUKTY</h2>
+            <table>
+                <tr>
+                    <td><img src="assets/smart-splash.jpg" alt=""></td>
+                    <td>soneque krem placeholder</td>
+                </tr>
+                <tr class="selected">
+                    <td><img src="assets/smart-splash.jpg" alt=""></td>
+                    <td>soneque krem placeholder</td>
+                </tr>
+                <tr>
+                    <td><img src="assets/smart-splash.jpg" alt=""></td>
+                    <td>soneque krem placeholder</td>
+                </tr>
+                <tr>
+                    <td><img src="assets/smart-splash.jpg" alt=""></td>
+                    <td>soneque krem placeholder</td>
+                </tr>
+                <tr>
+                    <td><img src="assets/smart-splash.jpg" alt=""></td>
+                    <td>soneque krem placeholder</td>
+                </tr>
+            </table>
+        </div>
+        <img src="assets/smart-splash.jpg" alt="">
+        <button>DODAJ DO KOSZYKA</button>
+        <div class="description">
+            <h2>soneque krem placeholder</h2>
+            <h3>873zł</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ipsum aperiam facilis ratione repellendus ea alias mollitia vel? Placeat explicabo at modi. Harum omnis non repellat totam culpa id placeat? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, nesciunt nemo voluptatibus voluptates quidem nam placeat tempore rem, magni molestiae mollitia expedita delectus, nihil ratione? Animi quidem totam quos ex. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis dolores, fugiat ut natus tempore, earum labore a velit, itaque ab quaerat cum delectus culpa quasi magni animi id ducimus officiis.</p>
+        </div>
+    </main>
+</body>
+</html>
